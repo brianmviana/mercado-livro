1f9940544abbfbd7823fd7aebcf7dc09fdd1e72b
Brian M. Viana <brianmviana@gmail.com>
2022-01-25 15:02:48 -0300

    iniciando controler e model consumer

---
+++ src/main/kotlin/dev/brianmviana/mercadolivro/model/Consumer.kt
@@ -0,0 +1,3 @@
+package dev.brianmviana.mercadolivro.model
+
+data class Consumer(var id : String, var name : String, var email : String)
---
+++ src/test/kotlin/dev/brianmviana/mercadolivro/MercadoLivroApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.brianmviana.mercadolivro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
